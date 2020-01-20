ff57fbf102242ece730c3568e5426b6a90b9f3c8
User <User@DESKTOP-DLNUQSL>
2020-01-20 23:02:57 +0900

    Add annotation

--- src/jun/calendar/Calendar.java
+++ src/jun/calendar/Calendar.java
@@ -6,9 +6,9 @@ public class Calendar {
 	
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int getMaxDaysOfMonth(int month) {
+	public int getMaxDaysOfMonth(int month) { // int로 입력받아서 int를 되돌려준다.
 		return MAX_DAYS[month - 1];
-	}
+	} // 기능을 하나의 함수로 만들어 밖으로 빼내었음.
 	
 	public void printSampleCalendar() {
 		System.out.println("일   월   화   수   목   금   토");
@@ -17,19 +17,19 @@ public class Calendar {
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-	}
+	} // 달력도 기능을 하나의 함수로 만들어 밖으로 빼내었음.
 	
 	public static void main(String[] args) {
 		
 		// 숫자를 입력받아 해당하는 달의 최대 일 수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
+		Calendar cal = new Calendar(); // Calendar 생성
 		System.out.println("달을 입력하세요.");
 		int month = scanner.nextInt();
 		
 //		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
-		System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month)); // cal에 있는 getMaxDaysOfMonth를 불러온다.
 		
 		cal.printSampleCalendar();
 		
