6165faa99061eab75d48bfd6c969050970af9e21
User <User@DESKTOP-DLNUQSL>
2020-01-29 00:44:59 +0900

    Repaired second line error

--- src/jun/calendar/Calendar3.java
+++ src/jun/calendar/Calendar3.java
@@ -1,6 +1,6 @@
 package jun.calendar;
 
-public class Calendar2 {
+public class Calendar3 {
 	
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 윤년에는 2월에 29일이 생기므로 새로운 배열을 하나 더 만들었다.
@@ -21,16 +21,40 @@ public class Calendar2 {
 	} // 기능을 하나의 함수로 만들어 밖으로 빼내었음.
 	  // 배열의 시작은 1부터가 아니라 0부터이므로 'month - 1'을 사용해주어야한다.
 	
-	public void printCalendar(int year, int month) {
+	public void printCalendar2(int year, int month, int weekday) {
 		System.out.printf("    <<%4d년 %3d월>>\n", year, month); // %d와 다르게 %4d라고 하면 4칸을 차지하는 정수라는 뜻 (Ex. 2020)
-		System.out.println("일   월   화   수   목   금   토");
-		System.out.println("--------------------");
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("------------------------");
+		
+		// 공백을 넣는 코드
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
 		
 		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int newline = (count < 7) ? count : 0;
 		
-		for(int i = 1; i <= maxDay; i++) {
+		/*
+		if (count < 7) { 				'위에 int newline = (count < 7) ? count : 0;' 와 똑같음
+			newline = count;
+		} else {
+			newline = 0;
+		}
+		*/
+		
+		// 첫번째 줄 프린트하기
+		for(int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+		}
+		System.out.println();
+		
+		// 두번째 줄 부터 마지막 줄 까지 프린트하기
+		
+		count++;
+		for(int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == newline) {
 				System.out.println();
 			}
 		}
@@ -39,9 +63,6 @@ public class Calendar2 {
 //		System.out.println("15 16 17 18 19 20 21");
 //		System.out.println("22 23 24 25 26 27 28");
 	} // 달력도 기능을 하나의 함수로 만들어 밖으로 빼내었음.
-	
-	public static void main(String[] args) {
-		
-	}
+
 
 }
---
+++ src/jun/calendar/Prompt2.java
@@ -0,0 +1,78 @@
+package jun.calendar;
+
+import java.util.Scanner;
+
+public class Prompt2 {
+	
+	/*
+	 * 0 = Sunday 부터 시작해서 6 = Saturday까지
+	 */
+	
+	public int parseDay(String week) {
+		if(week.equals("su")) {
+			return 0;
+		} else if(week.equals("mo")) {
+			return 1;
+		} else if(week.equals("tu")) {
+			return 2;
+		} else if(week.equals("we")) {
+			return 3;
+		} else if(week.equals("th")) {
+			return 4;
+		} else if(week.equals("fr")) {
+			return 5;
+		} else if(week.equals("sa")) {
+			return 6;
+		} else {
+			return 0;
+		}
+	}
+	
+	public void runPrompt() {
+
+		// 숫자를 입력받아 해당하는 달의 최대 일 수를 출력하는 프로그램
+		
+		Scanner scanner = new Scanner(System.in);
+		Calendar3 cal = new Calendar3(); // Calendar 생성
+		
+		int month = 1;
+		int year = 2020;
+		int weekday = 0;
+		
+		while (true) {
+			System.out.println("\n연도를 입력하세요. (-1을 입력하면 종료)");
+			System.out.print("YEAR> ");
+			year = scanner.nextInt();
+			if (year == -1) {
+				break;
+			}
+			System.out.println("달을 입력하세요");
+			System.out.print("MONTH> ");
+			month = scanner.nextInt();
+			System.out.println("첫번째 요일을 입력하세요. (su, mo, tu, we, th, fr, sa).");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
+			
+//			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month)); // cal에 있는 getMaxDaysOfMonth를 불러온다.
+			
+			cal.printCalendar2(year, month, weekday);
+		} // 원하는 만큼 반복횟수를 입력 받은 다음 입력한 숫자 만큼 반복하여 해당하는 달의 최대 일 수를 출력하도록 변경.
+		
+//		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.println("Have a nice day!");
+		scanner.close();
+		
+	}
+
+	public static void main(String[] args) {
+		// 셸 실행
+		Prompt2 p = new Prompt2();
+		p.runPrompt();
+	}
+
+}
