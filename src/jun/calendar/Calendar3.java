501762da44def894ffda9b7a929a323497d1bf4f
User <User@DESKTOP-DLNUQSL>
2020-01-30 01:59:06 +0900

    Implement printing calendar (auto print weekday version)

--- src/jun/calendar/Calendar3.java
+++ src/jun/calendar/Calendar3.java
@@ -2,8 +2,8 @@ package jun.calendar;
 
 public class Calendar3 {
 	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 윤년에는 2월에 29일이 생기므로 새로운 배열을 하나 더 만들었다.
+	private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 윤년에는 2월에 29일이 생기므로 새로운 배열을 하나 더 만들었다.
 	
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
@@ -14,17 +14,20 @@ public class Calendar3 {
 	}
 	public int getMaxDaysOfMonth(int year, int month) { // int로 입력받아서 int를 되돌려준다.
 		if (isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month - 1];
+			return LEAP_MAX_DAYS[month];
 		} else {
-			return MAX_DAYS[month - 1];
+			return MAX_DAYS[month];
 		}
 	} // 기능을 하나의 함수로 만들어 밖으로 빼내었음.
 	  // 배열의 시작은 1부터가 아니라 0부터이므로 'month - 1'을 사용해주어야한다.
 	
-	public void printCalendar2(int year, int month, int weekday) {
-		System.out.printf("    <<%4d년 %3d월>>\n", year, month); // %d와 다르게 %4d라고 하면 4칸을 차지하는 정수라는 뜻 (Ex. 2020)
+	public void printCalendar2(int year, int month) {
+		System.out.printf("    <<%d년 %d월>>\n", year, month); // %d와 다르게 %4d라고 하면 4칸을 차지하는 정수라는 뜻 (Ex. 2020)
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("------------------------");
+		
+		// 요일을 자동으로 받아오는 코드
+		int weekday = getWeekday(year, month, 1);
 		
 		// 공백을 넣는 코드
 		for (int i = 0; i < weekday; i++) {
@@ -63,6 +66,28 @@ public class Calendar3 {
 //		System.out.println("15 16 17 18 19 20 21");
 //		System.out.println("22 23 24 25 26 27 28");
 	} // 달력도 기능을 하나의 함수로 만들어 밖으로 빼내었음.
+	
+	private int getWeekday(int year, int month, int day) {
+		int standardyear = 1970;
+		final int STANDARD_WEEKDAY = 3; // 1970년 1월 1일이 목요일이기 때문에 3을 기준으로 잡음.
+		
+		int count = 0;
+		
+		for(int i = standardyear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365; // 윤년
+			count += delta;
+		}
+		
+		for(int i = 0; i < month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+		
+		count += day;
+		
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+		return weekday;
+	}
 
 
 }
--- src/jun/calendar/Prompt2.java
+++ src/jun/calendar/Prompt2.java
@@ -37,7 +37,6 @@ public class Prompt2 {
 		
 		int month = 1;
 		int year = 2020;
-		int weekday = 0;
 		
 		while (true) {
 			System.out.println("\n연도를 입력하세요. (-1을 입력하면 종료)");
@@ -49,9 +48,9 @@ public class Prompt2 {
 			System.out.println("달을 입력하세요");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-			System.out.println("첫번째 요일을 입력하세요. (su, mo, tu, we, th, fr, sa).");
-			String str_weekday = scanner.next();
-			weekday = parseDay(str_weekday);
+//			System.out.println("첫번째 요일을 입력하세요. (su, mo, tu, we, th, fr, sa).");
+//			String str_weekday = scanner.next();
+//			weekday = parseDay(str_weekday); >>> 요일을 자동으로 입력하게 수정할 것이므로 주석처리.
 
 			if (month > 12 || month < 1) {
 				System.out.println("잘못된 입력입니다.");
@@ -60,7 +59,7 @@ public class Prompt2 {
 			
 //			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month)); // cal에 있는 getMaxDaysOfMonth를 불러온다.
 			
-			cal.printCalendar2(year, month, weekday);
+			cal.printCalendar2(year, month);
 		} // 원하는 만큼 반복횟수를 입력 받은 다음 입력한 숫자 만큼 반복하여 해당하는 달의 최대 일 수를 출력하도록 변경.
 		
 //		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
